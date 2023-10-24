4dda40174f0a9e7579677811865ed9189260f193
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2023-10-24 14:23:39 +0530

    Manual spot list

---
+++ scripts/spots.js
@@ -0,0 +1,24 @@
+// put all the HTML for the spots page in the div with class "container"
+const container = document.createElement('div')
+container.className = 'container'
+document.body.appendChild(container)
+
+// HTML for header with title "List of Spots"
+const header = document.createElement('h1')
+header.innerText = 'List of Spots'
+container.appendChild(header)
+
+// define list of spots
+const spot_list = [1, 9, 11, 15, 17, 21, 22, 30, 31, 32, 36, 39, 43, 44, 47,
+    51, 53, 54, 63, 66, 68, 79, 89, 92, 94, 103]
+
+// for each spot, show a list item with the text "Spot #" where # is the spot number
+// and with a hyperlink to the spot's page, which is the "spot#.html" file in the "spots" folder
+spot_list.forEach(function (spot) {
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+    a.href = `spots/spot${spot}.html`
+    a.innerText = `Spot ${spot}`
+    li.appendChild(a)
+    container.appendChild(li)
+})
