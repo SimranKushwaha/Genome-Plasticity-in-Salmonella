245b9747c7e25162d71108c2e59beca214f37aa9
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2023-11-08 13:10:33 +0530

    Some formatting changes

--- scripts/spots.js
+++ scripts/spots.js
@@ -7,11 +7,30 @@ const spot_list = [1, 9, 11, 15, 17, 21, 22, 30, 31, 32, 36, 39, 43, 44, 47,
 
 // for each spot, show a list item with the text "Spot #" where # is the spot number
 // and with a hyperlink to the spot's page, which is the "spot#.html" file in the "spots" folder
-spot_list.forEach(function (spot) {
-    const li = document.createElement('li')
-    const a = document.createElement('a')
-    a.href = `spots/spot${spot}.html`
-    a.innerText = `Spot ${spot}`
-    li.appendChild(a)
-    parent.appendChild(li)
-})
+// Each hyperlink is quite small. We can show four hyperlinks per row.
+// The parent container div is used to show the list.
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+let row, cell, link, spotNum;
+
+for (let i = 0; i < spot_list.length; i++) {
+    if (i % 4 === 0) {
+        row = document.createElement('tr');
+        tbody.appendChild(row);
+    }
+
+    cell = document.createElement('td');
+    link = document.createElement('a');
+    spotNum = spot_list[i];
+
+    link.setAttribute('href', `spots/spot${spotNum}.html`);
+    link.textContent = `Spot ${spotNum}`;
+
+    cell.appendChild(link);
+    row.appendChild(cell);
+}
+
+table.appendChild(tbody);
+table.style.borderCollapse = 'collapse';
+table.style.border = 'none';
+parent.appendChild(table);
